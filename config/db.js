df1e1af192c6fb2bc60c578882b8e61ded3bb194
ronycse16b <rony.cse16b@gmail.com>
2023-04-16 09:09:22 +0600

    ghj

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
     try {
         
-        const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qnzupqp.mongodb.net/tally-app`);
+        const conn = await mongoose.connect(`mongodb+srv://dbuser-rony-16b:DL825Q5kksIWkQtB@cluster0.qnzupqp.mongodb.net/tally-app`);
         console.log(`Connected to MongoDB database ${conn.connection.host}`.bgMagenta.white);
 
     } catch (error) {
